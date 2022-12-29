10958928db68accf06dadec395bf0010e776867c
jcz2 <jcz2@users.noreply.github.com>
2022-12-29 22:24:29 +0100

    add todo list v1

---
+++ src/main/scala/TodoList.scala
@@ -0,0 +1,40 @@
+import java.util.UUID.randomUUID
+import scala.collection.mutable
+
+case class Todo(id: String, description: String, completed: Boolean)
+
+class TodoList {
+  private val todos = mutable.Map[String, Todo]()
+
+  def add(description: String): Todo = {
+    val todo = Todo(randomUUID.toString, description, completed = false)
+    todos.addOne(todo.id, todo)
+    todo
+  }
+
+  def remove(id: String): Unit = todos.remove(id)
+
+  def update(id: String, completed: Boolean): Unit = {
+    val todo = todos.get(id)
+    if (todo.isDefined) {
+      todos.update(id, todo.get.copy(completed = completed))
+    }
+  }
+
+  def check(id: String): Unit = update(id, completed = true)
+
+  def uncheck(id: String): Unit = update(id, completed = false)
+
+  def isCompleted: Boolean = todos.forall { case (_, todo) => todo.completed }
+
+  def clear(): Unit = todos.clear()
+}
+
+object Main extends App {
+  val todoList = new TodoList()
+  val todo = todoList.add("Make lunch")
+  todoList.check(todo.id)
+  if (todoList.isCompleted) {
+    todoList.clear()
+  }
+}
